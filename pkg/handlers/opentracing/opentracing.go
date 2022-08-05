8d094263e6c01ca34100a45438dee10c18588ff3
Sofyan Saputra <sofyan.saputra@otoraja.co.id>
2022-08-05 15:00:39 +0700

    route path

--- pkg/handlers/opentracing/opentracing.go
+++ pkg/handlers/opentracing/opentracing.go
@@ -5,7 +5,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/sqs"
 	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
-	mq "github.com/orn-id/mq-router"
+	mq "github.com/sofyan48/mq-router"
 )
 
 var DefaultSpanName = "sqs.message"
